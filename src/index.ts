4d8734b6607a48797e7a42a3736b4af5bdca6377
genka <tamani.coding@gmail.com>
2021-05-30 13:04:49 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ import { MaskPass, ClearMaskPass } from 'three/examples/jsm/postprocessing/MaskP
 import { TexturePass } from 'three/examples/jsm/postprocessing/TexturePass.js';
 import { AfterimagePass } from 'three/examples/jsm/postprocessing/AfterimagePass.js';
 import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js';
+import { HalftonePass } from 'three/examples/jsm/postprocessing/HalftonePass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
 import { BleachBypassShader } from 'three/examples/jsm/shaders/BleachBypassShader.js';
 import { ColorifyShader } from 'three/examples/jsm/shaders/ColorifyShader.js';
@@ -25,6 +27,11 @@ import { SepiaShader } from 'three/examples/jsm/shaders/SepiaShader.js';
 import { VignetteShader } from 'three/examples/jsm/shaders/VignetteShader.js';
 import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
 import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader.js';
+import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js';
+import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
+import { GodRaysFakeSunShader, GodRaysDepthMaskShader, GodRaysCombineShader, GodRaysGenerateShader } from 'three/examples/jsm/shaders/GodRaysShader.js';
+import { PixelShader } from 'three/examples/jsm/shaders/PixelShader.js';
+import { SobelOperatorShader } from 'three/examples/jsm/shaders/SobelOperatorShader.js';
 
 // CAMERA
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1500);
@@ -49,7 +56,7 @@ tweenCamera2.onUpdate(updateCamera)
 // tweenCamera1.start()
 
 // RENDERER
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
@@ -101,6 +108,45 @@ composer.addPass( renderPass );
 //   height: window.innerHeight
 // } )
 // composer.addPass(bokehPass)
+// const halfTonePass = new HalftonePass(window.innerWidth, window.innerHeight, {
+//   shape: 1,
+//   radius: 4,
+//   rotateR: Math.PI / 12,
+//   rotateB: Math.PI / 12 * 2,
+//   rotateG: Math.PI / 12 * 3,
+//   scatter: 0,
+//   blending: 1,
+//   blendingMode: 1,
+//   greyscale: false,
+//   disable: false
+// })
+// composer.addPass(halfTonePass)
+// const pixelPass = new ShaderPass( PixelShader );
+// pixelPass.uniforms[ "resolution" ].value = new THREE.Vector2( window.innerWidth, window.innerHeight );
+// pixelPass.uniforms[ "resolution" ].value.multiplyScalar( window.devicePixelRatio );
+// pixelPass.uniforms[ "pixelSize" ].value = 8;
+// console.log(window.devicePixelRatio)
+// composer.addPass( pixelPass );
+// SOBEL EDGE DETECTION
+// const effectGrayScale = new ShaderPass( LuminosityShader );
+// composer.addPass( effectGrayScale );
+// const effectSobel = new ShaderPass( SobelOperatorShader );
+// effectSobel.uniforms[ 'resolution' ].value.x = window.innerWidth * window.devicePixelRatio;
+// effectSobel.uniforms[ 'resolution' ].value.y = window.innerHeight * window.devicePixelRatio;
+// composer.addPass( effectSobel );
+// BLOOM
+// const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 )
+// const params = {
+//   exposure: 0.9,
+//   bloomStrength: 1.5,
+//   bloomThreshold: 0.5,
+//   bloomRadius: 0
+// };
+// renderer.toneMappingExposure = Math.pow( params.exposure, 4.0 );
+// bloomPass.threshold = params.bloomThreshold;
+// bloomPass.strength = params.bloomStrength;
+// bloomPass.radius = params.bloomRadius;
+// composer.addPass(bloomPass)
 
 export function animate() {
   TWEEN.update();
